fcd179d510c9a74fbec3001be5bf3e528abf6a55
Misael <misael.delamorena@gmail.com>
2025-12-01 22:07:34 +0100

    PrimerComponente : Contador

---
+++ MiPrimerComponenteAngular/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ContadorComponent } from './components/contador/contador.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: ContadorComponent,
+  },
+];
---
+++ MiPrimerComponenteAngular/src/app/components/contador/contador.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-contador',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './contador.component.html',
+})
+export class ContadorComponent {
+  contador: number = 0;
+  incrementar() {
+    this.contador++;
+  }
+  decrementar() {
+    this.contador--;
+  }
+  reiniciar() {
+    this.contador = 0;
+  }
+}
